5b63c7068f336c719a692bdaa2216716ceafe036
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-07-26 10:47:26 +0200

    Fix hold type mapping for akimbo weapons

---
+++ lua/weapontweakdata.lua
@@ -0,0 +1,3 @@
+Hooks:PostHook(WeaponTweakData, "init", "init_shadow", function (self)
+	WeaponShadows:setup(self)
+end)
--- mod.lua
+++ mod.lua
@@ -2,40 +2,43 @@ if not WeaponShadows then
 
 	WeaponShadows = {}
 	WeaponShadows.mod_path = ModPath
+	WeaponShadows.akimbo_mappings = {}
+	WeaponShadows.id_redirects = {
+		aa12 = Idstring("s_assault_rifle"),
+		saiga = Idstring("s_assault_rifle")
+	}
+	WeaponShadows.hold_redirects = {
+		uzi = Idstring("s_uzi"),
+		bullpup = Idstring("s_bullpup")
+	}
 
-	local ids_shadow_unit = Idstring("units/payday2/weapons/shadow/shadow")
-	local weapon_id_redirects = {
-		aa12 = "s_assault_rifle",
-		saiga = "s_assault_rifle"
-	}
-	local weapon_hold_redirects = {
-		uzi = "s_uzi",
-		bullpup = "s_bullpup"
-	}
-	local akimbo_mappings = {}
-	for k, v in pairs(WeaponTweakData.get_akimbo_mappings()) do
-		akimbo_mappings[v] = k
+	function WeaponShadows:setup(weapon_tweak_data)
+		self.npc_mappings = weapon_tweak_data:get_npc_mappings()
+		for k, v in pairs(weapon_tweak_data:get_akimbo_mappings()) do
+			self.akimbo_mappings[v] = k
+		end
 	end
 
+	local ids_shadow_unit = Idstring("units/payday2/weapons/shadow/shadow")
 	function WeaponShadows:spawn_shadow_unit(weapon_base)
 		weapon_base._shadow_unit = World:spawn_unit(ids_shadow_unit, weapon_base._unit:position(), weapon_base._unit:rotation())
 		weapon_base._unit:link(weapon_base._unit:orientation_object():name(), weapon_base._shadow_unit, weapon_base._shadow_unit:orientation_object():name())
 
-		local name = weapon_base._name_id and weapon_base._name_id:gsub("_npc$", ""):gsub("_crew$", "")
-		name = akimbo_mappings[name] or name
+		local short_name = weapon_base._name_id:gsub("_npc$", ""):gsub("_crew$", "")
+		local name = self.npc_mappings[self.akimbo_mappings[short_name]] or weapon_base._name_id
 		local tweak = tweak_data.weapon[name] or weapon_base:weapon_tweak_data()
 
 		 -- Check for a weapon specific shadow object first
-		local shadow_obj = weapon_base._shadow_unit:get_object(Idstring(weapon_id_redirects[name] or ("s_" .. name)))
+		local shadow_obj = weapon_base._shadow_unit:get_object(self.id_redirects[short_name] or Idstring("s_" .. short_name))
 
 		-- If there's no weapon specific shadow object, check for a hold specific shadow object
 		if not shadow_obj and tweak.hold then
-			if weapon_hold_redirects[tweak.hold] then
-				shadow_obj = weapon_base._shadow_unit:get_object(Idstring(weapon_hold_redirects[tweak.hold]))
+			if self.hold_redirects[tweak.hold] then
+				shadow_obj = weapon_base._shadow_unit:get_object(self.hold_redirects[tweak.hold])
 			elseif type(tweak.hold) == "table" then
 				for _, v in ipairs(tweak.hold) do
-					if weapon_hold_redirects[v] then
-						shadow_obj = weapon_base._shadow_unit:get_object(Idstring(weapon_hold_redirects[v]))
+					if self.hold_redirects[v] then
+						shadow_obj = weapon_base._shadow_unit:get_object(self.hold_redirects[v])
 						break
 					end
 				end
