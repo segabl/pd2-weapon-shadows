a22f24b1d84cd460391c115afb2a94aadef57800
Sebastian Gabl <sebastian.gabl@gmx.at>
2022-07-27 09:38:10 +0200

    Fix weapon mapping with NPC Weapon Customizer

--- mod.lua
+++ mod.lua
@@ -24,8 +24,9 @@ if not WeaponShadows then
 		weapon_base._shadow_unit = World:spawn_unit(ids_shadow_unit, weapon_base._unit:position(), weapon_base._unit:rotation())
 		weapon_base._unit:link(weapon_base._unit:orientation_object():name(), weapon_base._shadow_unit, weapon_base._shadow_unit:orientation_object():name())
 
-		local short_name = weapon_base._name_id:gsub("_npc$", ""):gsub("_crew$", "")
-		local name = self.npc_mappings[self.akimbo_mappings[short_name]] or weapon_base._name_id
+		local name_id = weapon_base._original_id or weapon_base._name_id
+		local short_name = name_id:gsub("_npc$", ""):gsub("_crew$", "")
+		local name = self.npc_mappings[self.akimbo_mappings[short_name]] or name_id
 		local tweak = tweak_data.weapon[name] or weapon_base:weapon_tweak_data()
 
 		 -- Check for a weapon specific shadow object first
