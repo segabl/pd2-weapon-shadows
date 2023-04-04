f89674a1f6837d94fba45b226b8e514fe6c97b7d
Sebastian Gabl <sebastian.gabl@gmx.at>
2023-04-04 17:27:33 +0200

    Fix shield shadows not working as client

--- lua/playerinventory.lua
+++ lua/playerinventory.lua
@@ -1,4 +1,4 @@
-Hooks:PostHook(CopInventory, "equip_shield", "equip_shield_shadow", function (self, shield_unit)
+Hooks:PostHook(PlayerInventory, "equip_shield", "equip_shield_shadow", function (self, shield_unit)
 	if alive(shield_unit) then
 		WeaponShadows:spawn_shield_shadow_unit(shield_unit:damage())
 	end
